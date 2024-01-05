d9b167fcc968ccc0e6b82d4479a5a09df56a1da9
Tushar <tushar.deomare@thinkitive.com>
2024-01-05 19:47:50 +0530

    Updated Day 1 Training Codes

---
+++ closure.js
@@ -0,0 +1,17 @@
+function outerFunction()
+{
+    let outerVariable = "I am present in outer function"
+
+    function insideFunction()
+    {
+        let insideVariable = "I am present in inner function"
+
+        console.log(outerVariable)
+    }
+
+    return insideFunction;
+}
+
+let closure = outerFunction();
+
+closure();
