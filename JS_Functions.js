7837a354ee354613280f5412ea37510b2fe99968
Tushar <tushar.deomare@thinkitive.com>
2024-01-08 20:15:24 +0530

    Feat: files after review

--- JS_Functions.js
+++ JS_Functions.js
@@ -1,41 +1,48 @@
-//Function Declaration
-function hello(name)
-{
-    console.log("Hello" + " " + name + "!");
-}
+// //Function Declaration
+// function hello(name)
+// {
+//     console.log("Hello" + " " + name + "!");
+// }
 
-hello("Tushar")
+// hello("Tushar")
 
-//Function Expression
-let hi = function(new_name)
-{
-    console.log("Hi" + " " + new_name +"!")
-}
+// //Function Expression
+// let hi = function(new_name)
+// {
+//     console.log("Hi" + " " + new_name +"!")
+// }
 
-hi("Tushar")
+// hi("Tushar")
 
-//Arrow Functions
-let hii=(naya_name)=>{
-    console.log("Hii" +" "+ naya_name +"!")
+// //Arrow Functions
+// let hii=(naya_name)=>{
+//     console.log("Hii" +" "+ naya_name +"!")
 
-}
+// }
 
-hii("Tushar")
+// hii("Tushar")
 
-//High Order function
-function operation(x, y, operationfunc)
-{
-    return operationfunc(x,y);
-}
+// //High Order function
+// function operation(x, y, operationfunc)
+// {
+//     return operationfunc(x,y);
+// }
 
-let add = (x,y)=>{
-    return x+y
-}
+// let add = (x,y)=>{
+//     return x+y
+// }
 
-let multiply = (x,y)=>{
-    return x*y
-}
+// let multiply = (x,y)=>{
+//     return x*y
+// }
 
-console.log(operation(5,3,add))
-console.log(operation(5,3,multiply))
+// console.log(operation(5,3,add))
+// console.log(operation(5,3,multiply))
 
+
+
+
+
+(function (a, b) {
+ console.log("Hello");
+})();
