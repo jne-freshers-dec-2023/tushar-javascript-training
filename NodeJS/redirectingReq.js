8b941ed7722c276bb70cddba17cd73868a737b15
Tushar <tushar.deomare@thinkitive.com>
2024-01-11 17:03:39 +0530

    redirected request to main page in nodejs

---
+++ NodeJS/redirectingReq.js
@@ -0,0 +1,30 @@
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer((req, res)=>{
+    const url = req.url;
+    const method = req.method;
+
+    if(url === '/'){
+        res.write('<html>')
+        res.write('<head><tittle>Enter Message</tittle></head>')
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')
+        res.write('</html>')
+        return res.end();
+    }
+
+    if(url === '/message' && method === 'POST'){
+        fs.writeFileSync('message.txt', 'Dummy text');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+    res.setHeader('Content-Type', 'text/html')
+    res.write("<html>")
+    res.write('<head><tittle>My First Page</tittle></head>')
+    res.write('<body><h1>Hello From My NodeJS Server</h1></body>')
+    res.write('</html>')
+
+})
+
+server.listen(3000);    
