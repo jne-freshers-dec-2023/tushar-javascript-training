9418d8a767eefdd28b65c2ad33f88a5d7c78f10d
Tushar <tushar.deomare@thinkitive.com>
2024-02-06 18:54:11 +0530

    react-training-day-5: finished food order app, hooks used in this app are useState, useEffect, useContext, useReducer

--- react-day-5/section-12/src/App.js
+++ react-day-5/section-12/src/App.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
-import Header from "./components/Layout/Header";
-import Meals from "./components/Meals/Meals";
-import Cart from "./components/Cart/Cart";
-import CartProvider from "./store/CartProvider";
+import { useState } from 'react';
+
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -14,10 +15,11 @@ function App() {
   const hideCartHandler = () => {
     setCartIsShown(false);
   };
+
   return (
     <CartProvider>
-      {cartIsShown && <Cart onClose={hideCartHandler}/>}
-      <Header onShowCart={showCartHandler}/>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
--- react-day-5/section-12/src/components/UI/Modal.js
+++ react-day-5/section-12/src/components/UI/Modal.js
@@ -1,9 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import classes from "./Modal.module.css";
+import { Fragment } from 'react';
+import ReactDOM from 'react-dom';
+
+import classes from './Modal.module.css';
 
 const Backdrop = (props) => {
-  return <div className={classes.backdrop} onClick={props.onClose}/>
+  return <div className={classes.backdrop} onClick={props.onClose}/>;
 };
 
 const ModalOverlay = (props) => {
@@ -14,14 +15,17 @@ const ModalOverlay = (props) => {
   );
 };
 
-const portalElement = document.getElementById("overlays");
+const portalElement = document.getElementById('overlays');
 
 const Modal = (props) => {
   return (
-    <>
-      {ReactDOM.createPortal(<Backdrop onClose={props.onClose}/>, portalElement)}
-      {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
-    </>
+    <Fragment>
+      {ReactDOM.createPortal(<Backdrop onClose={props.onClose} />, portalElement)}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </Fragment>
   );
 };
 
