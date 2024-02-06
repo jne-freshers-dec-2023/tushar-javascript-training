100c7e7fa5256211dd3220237f59d086af1aeacd
Tushar <tushar.deomare@thinkitive.com>
2024-02-06 19:45:57 +0530

    react-training-day-5: use useCallback hook to a function to not re-render for every reload

--- react-day-5/section-13/src/App.js
+++ react-day-5/section-13/src/App.js
@@ -1,19 +1,20 @@
-import React, { useState } from 'react';
-import Button from './components/UI/Button/Button'
+import React, { useCallback, useState } from "react";
+import Button from "./components/UI/Button/Button";
 
-import './App.css';
-import DemoOutput from './components/Demo/DemoOutput';
+import "./App.css";
+import DemoOutput from "./components/Demo/DemoOutput";
 
 function App() {
-  const [showParagraph, setShowParagraph] = useState(false)
+  const [showParagraph, setShowParagraph] = useState(false);
 
-  const toggleParagraphHandler=()=>{
-    setShowParagraph((prevShowParagraph)=> !prevShowParagraph);
-  }
+  const toggleParagraphHandler = useCallback(() => {
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  }, []);
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={showParagraph}/>
+      <DemoOutput show={showParagraph} />
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
     </div>
   );
