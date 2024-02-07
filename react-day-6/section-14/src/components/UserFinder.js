0ca86bd72825440f0d5192fcf3afe62a111fa5b3
Tushar <tushar.deomare@thinkitive.com>
2024-02-07 13:22:35 +0530

    react-training-day-6: added error boundry to users component by using lifecycle method

--- react-day-6/section-14/src/components/UserFinder.js
+++ react-day-6/section-14/src/components/UserFinder.js
@@ -1,8 +1,9 @@
-import { Fragment, useState, useEffect, Component } from 'react';
+import { Fragment, useState, useEffect, Component } from "react";
 
-import Users from './Users';
-import classes from './UserFinder.module.css';
-import UsersContext from '../store/users-context';
+import Users from "./Users";
+import classes from "./UserFinder.module.css";
+import UsersContext from "../store/users-context";
+import ErrorBoundary from './ErrorBoundary'
 
 class UserFinder extends Component {
   static contextType = UsersContext;
@@ -11,7 +12,7 @@ class UserFinder extends Component {
     super();
     this.state = {
       filteredUsers: [],
-      searchTerm: '',
+      searchTerm: "",
     };
   }
 
@@ -38,9 +39,11 @@ class UserFinder extends Component {
     return (
       <Fragment>
         <div className={classes.finder}>
-          <input type='search' onChange={this.searchChangeHandler.bind(this)} />
+          <input type="search" onChange={this.searchChangeHandler.bind(this)} />
         </div>
-        <Users users={this.state.filteredUsers} />
+        <ErrorBoundary>
+          <Users users={this.state.filteredUsers} />
+        </ErrorBoundary>
       </Fragment>
     );
   }
