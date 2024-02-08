7c46a640435bd1969924c4d7ea4bb539ed27fb45
Tushar <tushar.deomare@thinkitive.com>
2024-02-08 19:41:57 +0530

    react-training-day-7: added multiple states in redux for react

--- react-day-7/section-19/src/components/Counter.js
+++ react-day-7/section-19/src/components/Counter.js
@@ -4,7 +4,11 @@ import { useSelector, useDispatch } from "react-redux";
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
-  const toggleCounterHandler = () => {};
+  const show = useSelector((state) => state.showCounter);
+
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
 
   const incrementHandler = () => {
     dispatch({ type: "increment" });
@@ -15,13 +19,13 @@ const Counter = () => {
   };
 
   const increaseHandler = () => {
-    dispatch({ type: "increase", amount: 5});
+    dispatch({ type: "increase", amount: 5 });
   };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={decrementHandler}>Decrement</button>
         <button onClick={incrementHandler}>Increment</button>
