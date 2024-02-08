88dd414ee47ea7cd4db65e029f50f6b3429d7890
Tushar <tushar.deomare@thinkitive.com>
2024-02-08 19:34:01 +0530

    react-training-day-7: attached payload to actions in redux

--- react-day-7/section-19/src/components/Counter.js
+++ react-day-7/section-19/src/components/Counter.js
@@ -6,23 +6,26 @@ const Counter = () => {
   const counter = useSelector((state) => state.counter);
   const toggleCounterHandler = () => {};
 
-  const incrementHandler=()=>{
-    dispatch({type: 'increment'})
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
 
-  }
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
 
-  const decrementHandler=()=>{
-    dispatch({type: 'decrement'})
-
-  }
+  const increaseHandler = () => {
+    dispatch({ type: "increase", amount: 5});
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
       <div>
-        <button onClick={incrementHandler}>Increment</button>
         <button onClick={decrementHandler}>Decrement</button>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
